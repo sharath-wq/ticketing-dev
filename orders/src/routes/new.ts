3d1e7e93cf75f7315158cb9a7a0b6ad26396eda4
sharath-wq <chandranpsharath@gmail.com>
2024-01-20 09:36:45 +0530

    version added

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -60,6 +60,7 @@ router.post(
                 id: ticket.id,
                 price: ticket.price,
             },
+            version: order.version,
         });
 
         res.status(201).send(order);
