9a3cbe73d8e7308ea5c22653795a1db448fd8621
sharath-wq <chandranpsharath@gmail.com>
2024-01-20 12:22:38 +0530

    added test cases for order-created listeners

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,8 @@ import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from '@scticketsco
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
 import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
+import mongoose from 'mongoose';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -22,6 +24,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
         // save the ticket
         await ticket.save();
+        await new TicketUpdatedPublisher(this.client).publish({
+            id: ticket.id,
+            version: ticket.version,
+            title: ticket.title,
+            price: ticket.price,
+            userId: ticket.userId,
+            orderId: ticket.orderId,
+        });
 
         // ack teh message
         msg.ack();
